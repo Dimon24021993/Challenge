4c137f862d18331da9b2d0088fd67dcdadbf14d6
DMITRIY <khmelnitskiy93@gmail.com>
2017-05-23 13:03:09 +0300

    First comit

---
+++ CloneTest/Clonable/Clonable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Clonable
+{
+    public static class Clonable 
+    {
+        public static T Clone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable) throw new ArgumentException("The class must have serializable attribute.", "source");
+            if (object.ReferenceEquals(source, null)) return default(T);
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
